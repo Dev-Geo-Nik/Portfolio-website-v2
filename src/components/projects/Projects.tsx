829caa33099cf3c2c7ba2dc0113fd51aed052252
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2023-01-16 12:05:03 +0100

    add projects,change theme colors

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -19,8 +19,26 @@ const Projects: React.FC = () => {
 			{/* <h4 className={styles.section_title}>Full-Stack Apps</h4> */}
 			<section className={styles.projects_container}>
 				<ProjectCard
+					name="payapi"
+					url="https://payapi-app.netlify.app/"
+					githubUrl="https://github.com/dev-george-nikolaidis/payapi"
+					imageElement={<StaticImage src="../../assets/images/projects/payapi.png" alt="" placeholder="tracedSVG" objectFit="fill" className={styles.image} />}
+				/>
+				<ProjectCard
+					name="Jobduddy"
+					url="https://jobduddy-app.netlify.app/"
+					githubUrl="https://github.com/dev-george-nikolaidis/jobbuddy"
+					imageElement={<StaticImage src="../../assets/images/projects/jobduddy.png" alt="" placeholder="tracedSVG" objectFit="fill" className={styles.image} />}
+				/>
+				<ProjectCard
+					name="Studio X"
+					url="https://studio-x-portfolio.netlify.app/"
+					githubUrl="https://github.com/dev-george-nikolaidis/studio-x"
+					imageElement={<StaticImage src="../../assets/images/projects/studio-x.png" alt="" placeholder="tracedSVG" objectFit="fill" className={styles.image} />}
+				/>
+				<ProjectCard
 					name="Hotburger"
-					url="https://hotburger-restaurant.netlify.app/"
+					url=""
 					githubUrl="https://github.com/Dev-Geo-Nik/HOTBURGER"
 					imageElement={<StaticImage src="../../assets/images/projects/hotburger.png" alt="" placeholder="tracedSVG" objectFit="fill" className={styles.image} />}
 				/>
@@ -29,6 +47,12 @@ const Projects: React.FC = () => {
 					url="https://marco-polo-portfolio-app.netlify.app/"
 					githubUrl="https://github.com/Dev-Geo-Nik/Marco-polo"
 					imageElement={<StaticImage src="../../assets/images/projects/marco-polo.png" alt="" placeholder="tracedSVG" objectFit="fill" className={styles.image} />}
+				/>
+				<ProjectCard
+					name="Photosnap"
+					url="https://photosnap-pro.netlify.app/"
+					githubUrl="https://github.com/dev-george-nikolaidis/photosnap"
+					imageElement={<StaticImage src="../../assets/images/projects/photosnap.png" alt="" placeholder="tracedSVG" objectFit="fill" className={styles.image} />}
 				/>
 				<ProjectCard
 					name="Space Tourism"
@@ -41,6 +65,12 @@ const Projects: React.FC = () => {
 					url="https://clock-portfolio-project.netlify.app/"
 					githubUrl="https://github.com/Dev-Geo-Nik/Clock"
 					imageElement={<StaticImage src="../../assets/images/projects/clock.png" alt="" placeholder="tracedSVG" objectFit="fill" className={styles.image} />}
+				/>
+				<ProjectCard
+					name="Cinemania"
+					url=""
+					githubUrl="https://github.com/dev-george-nikolaidis/Cinemania"
+					imageElement={<StaticImage src="../../assets/images/projects/cinemania.png" alt="" placeholder="tracedSVG" objectFit="fill" className={styles.image} />}
 				/>
 			</section>
 		</section>
--- src/util/Themes.tsx
+++ src/util/Themes.tsx
@@ -1,3 +1,3 @@
 export const defaultTheme = "--clr-primary-1:#085461; --clr-primary-5:#0e95ad; --clr-image-container: #0f879c;";
-export const darkTheme = " --clr-primary-5:#000; 	--clr-shape: #b9b1ae;  --clr-image-container: #292629;";
+export const darkTheme = " --clr-primary-5:#000; 	--clr-shape: #307a88;  --clr-image-container: #292629;";
 // 685b55
